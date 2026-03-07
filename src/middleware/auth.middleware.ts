82fc266f9d3747aea0eb6c55543d6bd0146e3610
Shubham Kumar <project@fathommarineconsultants.com>
2026-03-07 17:16:46 +0530

    core: add global JWT auth middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,30 @@
+import {Request, Response, NextFunction} from 'express'
+import jwt from 'jsonwebtoken';
+
+export interface AuthRequest extends Request{
+    user?: {
+        userId: string;
+    };
+}
+
+export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction)=>{
+
+    try {
+        const authHeader = req.headers.authorization || req.headers.Authorization as string;
+
+        if(!authHeader.startsWith('Bearer')){
+            return res.status(401).json({error: 'Access denied. No token provided'});
+        }
+        // Extract just the token part
+        const token = authHeader.split(' ')[1];
+        // Verify the token using your secret key
+        const decoded = jwt.verify(token,process.env.JWT_secret as string) as {userId: string};
+
+        //Attaching decoded payload
+        req.user = decoded;
+        //  Pass control to the next function
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: 'Invalid or expired access token.' });
+    }
+}
